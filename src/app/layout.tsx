cec7cabea75b9ea907e74159b7e66d8b1cb4d47a
4regab <4regab@gmail.com>
2025-12-27 20:47:54 +0800

    chore: move Google verification file and add Android chrome icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,12 @@ export const metadata: Metadata = {
       { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
     ],
     apple: '/apple-touch-icon.png',
+    other: [
+      { rel: 'icon', url: '/android-chrome-192x192.png', sizes: '192x192' },
+    ],
   },
 };
 
